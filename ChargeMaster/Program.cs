9122349e597f90595d44cd6f40ce5506888063de
Thomas Bjork <thomas.bjork@outlook.com>
2026-02-16 08:46:10 +0100

    Remove HTTPS support and HSTS enforcement
    Removed HTTPS redirection and HSTS middleware from Program.cs. Also deleted the HTTPS profile from launchSettings.json, so the app now runs only over HTTP.

--- ChargeMaster/Program.cs
+++ ChargeMaster/Program.cs
@@ -109,12 +109,9 @@ namespace ChargeMaster
                 else
                 {
                     app.UseExceptionHandler("/Error");
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
                 }
 
                 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-                app.UseHttpsRedirection();
 
                 app.UseAntiforgery();
 
